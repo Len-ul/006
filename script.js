a779a4344937ff9774702efa8d3e51d2ba5ee981
lijianhao <3642490137@qq.com>
2025-02-10 12:14:56 +0800

    third commit

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+function getExactTime(time){
+    let date=new Date(time);
+    let year=date.getFullYear()+'-';
+    let month=(date.getMonth()+1<10?'0'+(date.getMonth()+1):date.getMonth()+1)+'-';
+    let dates=date.getDate()+' ';
+    let hour=date.getHours()+':';
+    let min=date.getMinutes()+':';
+    let second=date.getSeconds();
+    return year+month+dates+hour+min+second;
+}
 function getTime(){
     axios({
         url:'https://f.m.suning.com/api/ct.do',
         method:'get',
     }).then(res=>{
-        console.log(res.data);
-        document.getElementById('time').innerText = res.data;
+        console.log(res);
+        document.getElementById('time').innerText=getExactTime(res.data.currentTime);
     }).catch(error=>{
         console.log(error);
     });
@@ -14,7 +24,7 @@ function register(){
     const username=document.getElementById('username').value;
     const password=document.getElementById('password').value;
     axios({
-        url:'',
+        url:'http://43.143.169.168:9090/user/register',
         method:'post',
         data:{
             username,
@@ -22,13 +32,15 @@ function register(){
         }
     }).then(res=>{
         console.log('注册报文',res);
+        alert("注册成功！");
     }).catch(error=>{
         console.log(error);
+        alert("注册失败！");
     });
 }
 function getUserList(){
     axios({
-        url:'',
+        url:'http://43.143.169.168:9090/user/all',
         method:'get',
     }).then(res=>{
         console.log('用户信息列表报文',res);
